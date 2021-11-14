2338b12be4d955f8615c67ea1248ffb6b99d3b04
EmilioHoffmann <emiliohoffmann@hotmail.com>
2021-11-14 11:12:56 -0300

    Adding viewModels to DI

--- app/src/main/java/com/udacity/shoestore/di/AppModule.kt
+++ app/src/main/java/com/udacity/shoestore/di/AppModule.kt
@@ -1,9 +1,13 @@
 package com.udacity.shoestore.di
 
 import com.udacity.shoestore.login.LoginViewModel
+import com.udacity.shoestore.shoe_list.ShoeListViewModel
+import com.udacity.shoestore.splash.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel { LoginViewModel() }
+    viewModel { SplashViewModel() }
+    viewModel { ShoeListViewModel() }
 }
